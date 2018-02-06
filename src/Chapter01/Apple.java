7ffba39dcc724e858f7f57f5661d7fc55f01d479
Varun Upadhyay <varun.u28@gmail.com>
2018-02-06 08:24:33 -0800

    Added an intro sorting example to Ch01

---
+++ src/Chapter01/Apple.java
@@ -0,0 +1,14 @@
+package Chapter01;
+
+public class Apple {
+
+    private final int weight;
+
+    public Apple(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
